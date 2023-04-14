d5c0afc9755c7ebace4bc95562464523fed9b42b
Anas Nadaf <65071663+anasnadaf@users.noreply.github.com>
2023-04-14 22:35:55 +0530

    Delete LeetCode directory

--- LeetCode/9. Palindrome Number/solution.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        '''
-        Compare the original string with the reversed string using the equality operator
-        If they are the same, it means x is a palindrome, so return True
-        Otherwise, return False 
-        '''
-        return str(x)==str(x)[::-1] 
